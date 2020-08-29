d67fd670b5333cdec124c0a60cf0860f5ad426b2
letung3105 <15053897+letung3105@users.noreply.github.com>
2020-08-29 18:07:05 +0700

    refactor

--- simple-parser/Main.hs
+++ simple-parser/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import           Text.ParserCombinators.Parsec
                                          hiding ( spaces )
 import           System.Environment
+import           Numeric
+import           Data.Char
 
 main :: IO ()
 main = do
@@ -35,29 +37,7 @@ data LispVal = Atom String
 
 -- | Parse a lisp expression
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
-
--- | Parse a lisp string
---
--- A string begins with a double quote mark, followed by any number of
--- non-quote character, and ends with a double quote mark
-parseString :: Parser LispVal
-parseString = do
-  char '"'
-  v <- many $ escapedChar <|> noneOf "\""
-  char '"'
-  return $ String v
-
-escapedChar :: Parser Char
-escapedChar = do
-  char '\\'
-  esc <- oneOf "\\\"nrt"
-  return $ case esc of
-    '\\' -> '\\'
-    '"'  -> '"'
-    'n'  -> '\n'
-    'r'  -> '\r'
-    't'  -> '\t'
+parseExpr = parseNumber <|> parseString <|> parseAtom
 
 -- | Parse a lisp atom
 --
@@ -72,6 +52,73 @@ parseAtom = do
     "#f" -> Bool False
     atom -> Atom atom
 
+-- | Parse a lisp string
+--
+-- A string begins with a double quote mark, followed by any number of
+-- non-quote character, and ends with a double quote mark
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  v <- many $ escapedChar <|> noneOf "\""
+  char '"'
+  return $ String v
+
+-- | Parse escaped character sequence in a string
+escapedChar :: Parser Char
+escapedChar = do
+  char '\\'
+  esc <- oneOf "\\\"nrt"
+  return $ case esc of
+    '\\' -> '\\'
+    '"'  -> '"'
+    'n'  -> '\n'
+    'r'  -> '\r'
+    't'  -> '\t'
+
 -- | Parse a lisp number
 parseNumber :: Parser LispVal
-parseNumber = Number . read <$> many1 digit
+parseNumber = withRadix <|> decLiteral
+
+-- | Parse number literals that are represented in other bases
+withRadix :: Parser LispVal
+withRadix = do
+  char '#'
+  radix <- oneOf "bodx"
+  case radix of
+    'b' -> binLiteral
+    'o' -> octLiteral
+    'd' -> decLiteral
+    'x' -> hexLiteral
+
+-- | Parse a number represented in binary
+binLiteral :: Parser LispVal
+binLiteral = do
+  ns <- many1 $ oneOf "01"
+  return $ Number $ binToInt ns
+ where
+  binToInt :: String -> Integer
+  binToInt ns = toInteger $ sum
+    [ bit * 2 ^ i | (bit, i) <- zip (map digitToInt ns) [ln - 1, ln - 2 ..] ]
+    where ln = length ns
+
+-- | Parse a number represented in octal
+octLiteral :: Parser LispVal
+octLiteral = do
+  ns <- many1 octDigit
+  case readOct ns of -- there should be no error
+    [(n, _)] -> return $ Number n
+
+-- | Parse a number represented in decimal
+decLiteral :: Parser LispVal
+decLiteral = do
+  ns <- many1 digit
+  case readDec ns of -- there should be no error
+    [(n, _)] -> return $ Number n
+
+-- | Parse a number represented in hexadecimal
+hexLiteral :: Parser LispVal
+hexLiteral = do
+  ns <- many1 hexDigit
+  case readHex ns of -- there should be no error
+    [(n, _)] -> return $ Number n
+
